41f67ac57f4efd79144b6a63a2ce8d998b145880
masanchezvega <masanchezvega@gmail.com>
2021-09-10 18:03:05 -0500

    add geo_location support

---
+++ functions/user_properties.js
@@ -0,0 +1,27 @@
+function getLocation() {
+    if (navigator.geolocation) {
+      pos = navigator.geolocation.getCurrentPosition(makePosition, makePositionError);
+    } else { 
+      pos = "Geolocation is not supported by this browser.";
+    }
+    return pos;
+  }
+  
+function makePosition(position) {
+  return  [position.coords.latitude, position.coords.longitude];
+}
+
+function makePositionError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      return "User denied the request for Geolocation."
+    case error.POSITION_UNAVAILABLE:
+      return "Location information is unavailable."
+    case error.TIMEOUT:
+      return "The request to get user location timed out."
+    case error.UNKNOWN_ERROR:
+      return "An unknown error occurred."
+  }
+}
+
+module.exports = { getLocation }
